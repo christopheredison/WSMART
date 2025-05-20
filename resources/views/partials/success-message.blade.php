cf8c69181de776e25721e0d8e83c8e45bf49c80c
christopherrwi <christopher@rwi.co.id>
2025-05-20 15:58:45 +0700

    Update Penilaian RMI - Aspek Dinamis

--- resources/views/partials/success-message.blade.php
+++ resources/views/partials/success-message.blade.php
@@ -17,3 +17,19 @@
   <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
+
+@if ($errors->any())
+<div class="alert alert-danger d-flex align-items-center mt-0 mb-xl-3" role="alert">
+  <div class="svg-icon svg-icon-danger">
+    @include('partials.icon-alert')
+  </div>
+  <div class="flex-1">
+    <ul class="mb-0">
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
