a8cf015f9e44b805f37f76cb87d99f9d4f63c279
Ahmad Syarifuz Zaki <syarifuzaki@gmail.com>
2025-11-13 14:48:42 +0700

    feat: Modul Risk Top Down and fixing PIC AP

--- resources/views/risk-register-ap/form-perencanaan-edit.blade.php
+++ resources/views/risk-register-ap/form-perencanaan-edit.blade.php
@@ -60,7 +60,7 @@
     </div>
     <div class="col-12 col-md-6">
         <div class="form-floating">
-            {{ Form::select('xpic', \App\Models\Jabatan::pluck('name', 'id'), '', ['class' => 'form-select', 'required']) }}
+            {{ Form::select('xpic', \App\Models\Jabatan::where('jabatan_type', 1)->pluck('name', 'id'), '', ['id' => 'picEdit', 'class' => 'form-select select2', 'required']) }}
             <label for="pic">PIC</label>
         </div>
     </div>
