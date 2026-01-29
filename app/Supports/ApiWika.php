896f9453664771d4dde20db06584d52bb84c4f19
Ahmad Syarifuz Zaki <syarifuzaki@gmail.com>
2026-01-29 10:22:42 +0700

    fix sync wika api

--- app/Supports/ApiWika.php
+++ app/Supports/ApiWika.php
@@ -96,14 +96,18 @@ class ApiWika
         ]);
         $result1 = $processData($result1);
 
-        // Ambil Data Bulan Ini
-        $result0 = $this->apiRequest('GET', 'proyek', [
-            'period' => date('Ym'),
-        ]);
-        $result0 = $processData($result0);
+        // // Ambil Data Bulan Ini
+        // $result0 = $this->apiRequest('GET', 'proyek', [
+        //     'period' => date('Ym'),
+        // ]);
+        // $result0 = $processData($result0);
 
         // Gabungkan
-        $result = array_merge($result2, $result1, $result0);
+        $result = array_merge(
+          $result2,
+          $result1,
+          // $result0
+        );
 
         return array_values($result);
     }
