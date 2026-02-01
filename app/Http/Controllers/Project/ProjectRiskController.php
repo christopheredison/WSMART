b7ff79abbafdad304a0a0b6975f2c363ae051bee
Syarifuz Zaki <syarifuzaki@gmail.com>
2026-02-01 08:54:46 +0700

    fix hide delete risiko

--- app/Http/Controllers/Project/ProjectRiskController.php
+++ app/Http/Controllers/Project/ProjectRiskController.php
@@ -294,9 +294,6 @@ class ProjectRiskController extends BasicCRUDController
             $this->tableLegend = [['icon' => '<span class="bx bx-show-alt"></span>', 'label' => 'View']];
             $this->tableActions[] = ['label' => '<span class="bx bx-show-alt" title="View"></span>', 'btn_icon' => true, 'action' => 'link', 'url' => route('projects.risks.view', ['project' => request()->route('project'), 'risk' => ':id']), 'title' => 'View Risiko'];
 
-            $this->tableLegend[] = ['icon' => '<span class="bx bx-trash text-danger"></span>', 'label' => 'Hapus'];
-            $this->tableActions[] = ['label' => '<span class="bx bx-trash text-danger"></span>', 'btn_icon' => true, 'action' => 'delete', 'url' => route('projects.risks.destroy', ['project' => request()->route('project'), 'risk' => ':id']), 'title' => 'Hapus', 'permissions' => ['project_risk_delete']];
-
             // CASE 1: RISK OFFICER PROJECT (Draft/Revisi)
             if (($status == DataBatch::STATUS_PROSES || $status == DataBatch::STATUS_REVISI) && $levelId == 6) {
                 $active_state = 'function(id, type, row) { return row.status === 1 || row.status === 5; }';
