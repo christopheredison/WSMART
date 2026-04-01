c3736fcc5f378a1c1202d03b0c05553ad08c4746
Ahmad Syarifuz Zaki <syarifuzaki@gmail.com>
2026-04-01 20:44:33 +0700

    feat: add perlakuan dampak risiko unit document

---
+++ app/Models/PerlakuanDampakRisikoUnitDocument.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class PerlakuanDampakRisikoUnitDocument extends Model
+{
+    protected $fillable = [
+        'perlakuan_dampak_risiko_unit_id',
+        'unit_risk_monitoring_id',
+        'user_id',
+        'file_name',
+        'file_path',
+        'mimetype',
+        'description',
+    ];
+
+    protected $casts = [
+        'file_name' => 'string',
+        'file_path' => 'string',
+        'mimetype' => 'string',
+        'description' => 'string',
+    ];
+
+    public $appends = [
+        'url',
+    ];
+
+    public function perlakuanDampakRisikoUnit()
+    {
+        return $this->belongsTo(PerlakuanDampakRisikoUnit::class);
+    }
+
+    public function unitRiskMonitoring()
+    {
+        return $this->belongsTo(UnitRiskMonitoring::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getUrlAttribute() {
+        return Storage::url($this->file_path);
+    }
+
+    public function delete()
+    {
+        Storage::delete($this->file_path);
+        return parent::delete();
+    }
+}
